3d77b8de71fbc68ba2a47693a2991442593aaa9e
Hossein Rahmatiniya <Hrahmatinia@gmail.com>
2020-02-03 23:00:24 +0000

    First contribution to my PHP Project

---
+++ TedSchool/Template.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html" charset="UTF-8">
+        <title><?php echo $title; ?> </title>
+        <link rel="stylesheet" type="text/css" href="Styles/StyleSheet.css"/>
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="banner">
+                
+            </div>
+            
+            <nav id="navigation">
+                <ul id="nav">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                </ul>
+            </nav>
+            
+            <div id="content-area">
+                <?php echo $content; ?>
+                
+            </div>
+            
+            <div id="sidebar">
+                
+            </div>
+            
+            <footer>
+                <p> All rights reserved </p>
+            </footer>
+        </div>
+       
+    </body>
+</html>
---
+++ TedSchool/index.php
@@ -0,0 +1,7 @@
+
+<?php
+$title = "Home";
+$content = "Helloooo";
+include 'Template.php';
+
+?>
